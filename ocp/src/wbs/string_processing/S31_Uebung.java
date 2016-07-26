4869c7f2bd8a6c841d97cbaf5ec66939f27baec0
ChrisBee <Chris@cokuss.de>
2016-07-26 15:38:49 +0200

    neuer Commit

--- ocp/src/wbs/string_processing/S31_Uebung.java
+++ ocp/src/wbs/string_processing/S31_Uebung.java
@@ -4,7 +4,7 @@ import java.util.Locale;
 
 // wo wird linksbündig ausgegeben?
 // wo wird rechtsbündig ausgegeben?
-// welchen einfluass hat das Locale auf dezimalpunkt / tausendertrennung?
+// welchen Einfluss hat das Locale auf Dezimalpunkt / Tausendertrennung?
 // was unterscheidet %f von %e?
 // wo erscheinen negative zahlen in runden klammern?
 // wo wird der gewöhnliche index verwendet, wo argindex?
@@ -16,24 +16,24 @@ public class S31_Uebung {
 
 	public static void main(String[] args) {
 
-		// Locale.setDefault(Locale.US);
-		// System.out.println(Locale.getDefault());
+		//Locale.setDefault(Locale.US);
+		System.out.println(Locale.getDefault());
 
-		System.out.printf("Der Preis ist: %.2f€%n", 5.99);
+		System.out.printf("Der Preis ist: %.2f €%n", 5.99);
 
-		System.out.printf("%s sagt: %d ist %2$dmal zuviel. %n", "Konfuzius", 100);
+		System.out.printf("%s sagt: %d ist %2$dmal zuviel. %n", "Konfuzius", 100, 200);
 
 		int i1 = -123;
 		int i2 = 12345;
 
-		System.out.printf(">%1$(7d< %n", i1);
+		System.out.printf(">%1$(7d< %n", i1);  // negative Werte in runde Klammern
 
 		System.out.printf(">%0,7d< %n", i2);
 
-		System.out.printf(">%+-7d< %n", i2);
+		System.out.printf(">%+09d< %n", i2);
 
-		System.out.printf(">%2$b + %1$5d< %n", i1, false);
+		System.out.printf(">%3$B + %1$5d + %<B< %n", i1, false, true); //das letzte genutzte Boolean %<B
 
-		System.out.printf(">%s + %S< %n", i1 + 1, "test");
+		System.out.printf(">%s + %S< %n", i1 + 1, "test"); // s und S scheint für jeden Datentyp zu klappen 
 	}
 }
