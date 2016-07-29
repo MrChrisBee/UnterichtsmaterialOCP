ae6a5f98ec1f75e93ce9f086fa072db59ea2aeba
ChrisBee <Chris@cokuss.de>
2016-07-29 14:44:18 +0200

    ein paar neue Übungen zu nio

--- ocp/src/wbs/nested_classes/NestetTested.java
+++ ocp/src/wbs/nested_classes/NestetTested.java
@@ -1,13 +1,14 @@
 package wbs.nested_classes;
 
+@SuppressWarnings("unused")
 public class NestetTested {
-	static int nestetInt = 1;
+	static int staticInstanceInt = 1;
 
 	public class MemberClass {
 		int memberInt = 2;
 
 		void memberM() {
-			System.out.println(nestetInt);
+			System.out.println(staticInstanceInt);
 		}
 	}
 
@@ -16,17 +17,20 @@ public class NestetTested {
 		class LocalClassInStatic {
 			void localStaticM() {
 				int normal = 4;
-				System.out.println(nestetInt);
+				System.out.println(staticInstanceInt);
 				System.out.println(inMain);
 				System.out.println(normal);
 			}
 		}
+		new LocalClassInStatic().localStaticM();
+		System.out.println("******************+++++++++++++++++************");
+		// wie komme ich von hier an localM() ???
 	}
  
 	public void murks() {
 		class LocalClass {
 			void localM() {
-				System.out.println(nestetInt);
+				System.out.println(staticInstanceInt);
 			}
 		}
 	}
--- ocp/src/wbs/nio/walkfiletree/WorkspaceStatistikDemo.java
+++ ocp/src/wbs/nio/walkfiletree/WorkspaceStatistikDemo.java
@@ -9,32 +9,31 @@ import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
-
-import javax.sound.midi.Sequence;
-import javax.sound.midi.Sequencer;
 
 /*
  * Wir ermitteln, wie viele Java-Source-Dateien sich in unserem Workspace befinden, 
  * und wie viel Speicherplatz sie belegen.
  */
 public class WorkspaceStatistikDemo {
+	static int anzahlDerDateien = 0;
+	static int belegterSpeicher = 0;
 
 	public static void main(String[] args) {
 		Path searchDir = Paths.get("C://Users//Master//git//UnterichtsmaterialOCP//ocp");
 		String javaDateien = "glob:*.java";
-		String ignoriereDies = "glob:bin"; // ist es irgenwie möglich .settings auszuschliessen?
+		String ignoriereDies = "glob:bin"; // ist es irgenwie möglich .settings
+											// auszuschliessen?
 		final PathMatcher matchFiles = FileSystems.getDefault().getPathMatcher(javaDateien);
 		final PathMatcher matchDirs = FileSystems.getDefault().getPathMatcher(ignoriereDies);
-		final AtomicInteger anzahlDerDateien = new AtomicInteger(0);
-		final AtomicLong belegterSpeicher = new AtomicLong(0);
 		try {
 			Files.walkFileTree(searchDir, new SimpleFileVisitor<Path>() {
 				@Override
 				public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
 					if (matchDirs.matches(dir.getFileName())) {
-						return FileVisitResult.SKIP_SUBTREE;
+						System.out.println("Suche in " + dir.getFileName() + " wird übersprungen!");
+						return FileVisitResult.SKIP_SUBTREE; 
+						//überspringt alle Treffer in Verzeichnissen
+						// die matchDirs entsprechen (bring nur einen Zeitvorteil)
 					}
 					return FileVisitResult.CONTINUE;
 				}
@@ -42,8 +41,8 @@ public class WorkspaceStatistikDemo {
 				@Override
 				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
 					if (matchFiles.matches(file.getFileName())) {
-						anzahlDerDateien.addAndGet(1);
-						belegterSpeicher.addAndGet(attrs.size());
+						anzahlDerDateien++;
+						belegterSpeicher += attrs.size();
 					}
 					return FileVisitResult.CONTINUE;
 				}
@@ -51,7 +50,7 @@ public class WorkspaceStatistikDemo {
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
 		}
-		System.out.println("Es gibt " + anzahlDerDateien + " Dateien, die " + belegterSpeicher + " kb belegen!" );
+		System.out.println("Es gibt " + anzahlDerDateien + " Dateien, die ca. " + (belegterSpeicher / 1024)
+				+ " Speicherplatz kb belegen!");
 	}
-
 }
