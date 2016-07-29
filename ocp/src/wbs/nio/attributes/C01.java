ba1dd920b909fe5f93e11e904f0f67fe41b25f9f
ChrisBee <Chris@cokuss.de>
2016-07-29 15:48:07 +0200

    fragen über fragen OCP nervt

--- ocp/src/wbs/nio/attributes/C01.java
+++ ocp/src/wbs/nio/attributes/C01.java
@@ -7,7 +7,7 @@ public class C01 {
 	public static void main(String[] args) {
 		Path path = Paths.get("resources\\io\\characterdata\\..\\..\\..\\..\\nasowas");
 		System.out.println(path.getNameCount()); // auch die .. (Wechsel) werden gezählt
-		System.out.println(path.subpath(0, 2));
+		System.out.println(path.subpath(0, 4));
 		System.out.println(path.normalize()); //gehe 3 hoch (3*..) der Rest ist die Ausgabe
 		System.out.println(path.normalize().subpath(0, 1));
 		System.out.println(path.normalize().getNameCount());
--- ocp/src/wbs/nio/attributes/C06.java
+++ ocp/src/wbs/nio/attributes/C06.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class C06 {
 	public static void main(String[] args) throws IOException {
@@ -14,6 +15,6 @@ public class C06 {
 		Files.deleteIfExists(path1);
 		Files.createFile(path1);
 		Files.copy(path1, path2);
-		Files.copy(path1, path2);
+		Files.copy(path1, path2 , StandardCopyOption.REPLACE_EXISTING);
 	}
 }
