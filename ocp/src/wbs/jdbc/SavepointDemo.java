27924daec80d0f6935f5ed58b8787df1793a8f10
ChrisBee <Chris@cokuss.de>
2016-08-09 15:44:53 +0200

    last 4 today

--- ocp/src/wbs/jdbc/SavepointDemo.java
+++ ocp/src/wbs/jdbc/SavepointDemo.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.sql.Savepoint;
 
 public class SavepointDemo {
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		String url = "jdbc:mysql://localhost:3306/jdbc";
 		String user = "root";
