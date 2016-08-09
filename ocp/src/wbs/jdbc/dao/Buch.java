cd49f38b56af0f3c9ab000c53744774d6a17741c
ChrisBee <Chris@cokuss.de>
2016-08-09 10:35:23 +0200

    jdbc annotiations  & rowset

--- ocp/src/wbs/jdbc/dao/Buch.java
+++ ocp/src/wbs/jdbc/dao/Buch.java
@@ -2,8 +2,11 @@ package wbs.jdbc.dao;
 
 import java.io.Serializable;
 
+import wbs.jdbc.annotation.Column;
+import wbs.jdbc.annotation.PrimaryKey;
+
 public class Buch implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	public Buch(String isbn, String titel, String autor, double preis) {
@@ -13,43 +16,56 @@ public class Buch implements Serializable {
 		this.autor = autor;
 		this.preis = preis;
 	}
+
 	public Buch() {
-			super();
+		super();
 	}
 
+	@Column
+	@PrimaryKey(index=1)
 	private String isbn = "";
+	@Column
 	private String titel = "";
+	@Column
 	private String autor = "";
+	@Column
 	private double preis = 0.0;
-	
+
 	public String getIsbn() {
 		return isbn;
 	}
+
 	public void setIsbn(String isbn) {
 		this.isbn = isbn;
 	}
+
 	public String getTitel() {
 		return titel;
 	}
+
 	public void setTitel(String titel) {
 		this.titel = titel;
 	}
+
 	public String getAutor() {
 		return autor;
 	}
+
 	public void setAutor(String autor) {
 		this.autor = autor;
 	}
+
 	public double getPreis() {
 		return preis;
 	}
+
 	public void setPreis(double preis) {
 		this.preis = preis;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Buch [isbn=" + isbn + ", titel=" + titel + ", autor=" + autor + ", preis=" + preis + "]";
 	}
-	
+
 }
